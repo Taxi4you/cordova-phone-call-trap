0cf56e06bc3c82ce99914819f39ab13cddc4343a
Taxi4you <41004207+Taxi4you@users.noreply.github.com>
2020-02-24 16:40:31 +0200

    Update PhoneCallTrap.java

--- src/android/PhoneCallTrap.java
+++ src/android/PhoneCallTrap.java
@@ -4,7 +4,6 @@ import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.PluginResult;
 import android.content.Context;
-import android.telephony.CallStateListener;
 import android.telephony.PhoneStateListener;
 import android.telephony.TelephonyManager;
 
@@ -26,62 +25,45 @@ public class PhoneCallTrap extends CordovaPlugin {
 
     private void prepareListener() {
         if (listener == null) {
-            listener = new ServiceReceiver();
+            listener = new CallStateListener();
             TelephonyManager TelephonyMgr = (TelephonyManager) cordova.getActivity().getSystemService(Context.TELEPHONY_SERVICE);
             TelephonyMgr.listen(listener, PhoneStateListener.LISTEN_CALL_STATE);
         }
     }
 }
 
-class ServiceReceiver extends BroadcastReceiver {
+class CallStateListener extends PhoneStateListener {
 
-    public void onReceive(final Context context, Intent intent) {
-        TelephonyManager telephony = (TelephonyManager)context.getSystemService(Context.TELEPHONY_SERVICE);
-        telephony.listen(new PhoneStateListener(){
-            @Override
-            public void onCallStateChanged(int state, String incomingNumber) {
-                super.onCallStateChanged(state, incomingNumber);
-                PluginResult result = new PluginResult(PluginResult.Status.OK, incomingNumber);
-                result.setKeepCallback(true);
+    private CallbackContext callbackContext;
 
-                callbackContext.sendPluginResult(result);
-            }
-        },PhoneStateListener.LISTEN_CALL_STATE);
+    public void setCallbackContext(CallbackContext callbackContext) {
+        this.callbackContext = callbackContext;
+    }
+
+    public void onCallStateChanged(int state, String incomingNumber) {
+        super.onCallStateChanged(state, incomingNumber);
+
+        if (callbackContext == null) return;
+
+        String msg = "";
+
+        switch (state) {
+            case TelephonyManager.CALL_STATE_IDLE:
+            msg = "IDLE";
+            break;
+
+            case TelephonyManager.CALL_STATE_OFFHOOK:
+            msg = "OFFHOOK";
+            break;
+
+            case TelephonyManager.CALL_STATE_RINGING:
+            msg = "RINGING";
+            break;
+        }
+
+        PluginResult result = new PluginResult(PluginResult.Status.OK, msg);
+        result.setKeepCallback(true);
+
+        callbackContext.sendPluginResult(result);
     }
 }
-
-// class CallStateListener extends PhoneStateListener {
-//
-//     private CallbackContext callbackContext;
-//
-//     public void setCallbackContext(CallbackContext callbackContext) {
-//         this.callbackContext = callbackContext;
-//     }
-//
-//     public void onCallStateChanged(int state, String incomingNumber) {
-//         super.onCallStateChanged(state, incomingNumber);
-//
-//         if (callbackContext == null) return;
-//
-//         String msg = "";
-//
-//         switch (state) {
-//             case TelephonyManager.CALL_STATE_IDLE:
-//             msg = "IDLE";
-//             break;
-//
-//             case TelephonyManager.CALL_STATE_OFFHOOK:
-//             msg = "OFFHOOK";
-//             break;
-//
-//             case TelephonyManager.CALL_STATE_RINGING:
-//             msg = "RINGING";
-//             break;
-//         }
-//
-//         PluginResult result = new PluginResult(PluginResult.Status.OK, incomingNumber);
-//         result.setKeepCallback(true);
-//
-//         callbackContext.sendPluginResult(result);
-//     }
-// }
