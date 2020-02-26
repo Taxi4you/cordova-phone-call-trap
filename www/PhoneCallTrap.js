2c4856fda7e0831cd9131735ff5cdfadd30f7a44
Taxi4you <41004207+Taxi4you@users.noreply.github.com>
2020-02-26 20:11:44 +0200

    Update PhoneCallTrap.js

--- www/PhoneCallTrap.js
+++ www/PhoneCallTrap.js
@@ -1,10 +1,5 @@
 var PhoneCallTrap = {
     
-    requestPermission: function(successCallback, errorCallback) {
-        errorCallback = errorCallback || this.errorCallback;
-        cordova.requestPermission(successCallback, errorCallback, 'PhoneCallTrap', 'requestPermission', []);
-    },
-    
     onCall: function(successCallback, errorCallback) {
         errorCallback = errorCallback || this.errorCallback;
         cordova.exec(successCallback, errorCallback, 'PhoneCallTrap', 'onCall', []);
